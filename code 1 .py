616237b52c1b28e9cf3c3f74dd56dc463c39de1d
Kino0131 <75887377+Kino0131@users.noreply.github.com>
2020-12-12 16:34:16 +0100

    Add files via upload

---
+++ code 1 .py
@@ -0,0 +1,19 @@
+
+with open("messagePourSauverLeMonde.txt","r") as fichier:
+    filin=fichier.read()
+
+print(filin)
+mot=["volcan"]
+i=0
+for i in range(80):
+    filin[i]
+#print(filin[i])
+    if filin[i]=="v":
+         if filin[i+1]=="o":
+            if filin[i+2]=="l":
+                if filin[i+3]=="c":
+                    if filin[i+4]=="a":
+                        if filin[i+5]=="n":
+                            print(f" A l'aide,tous aux abris !")
+
+    
